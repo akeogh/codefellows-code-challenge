d252aa4a850fa5fd7d6b8810c7c568bb3b16601e
Amanda Keogh <amanda.keogh@gmail.com>
2015-07-17 16:16:11 -0700

    Create server.js
    A simple node server.

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(request, response) {
+
+  var respond = function(code, body) {
+    response.writeHead(code, {'Content-Type': 'text/html'});
+    response.end(body);
+  };
+
+  var html = fs.readFile('index2.html', 'utf8', function(error, data) {
+    if (error) {
+      respond(500, 'Something went wrong! ' + error.toString());
+    } else {
+      respond(200, data);
+    }
+  });
+}).listen(3000);
+console.log("Server listening on port 3000");
